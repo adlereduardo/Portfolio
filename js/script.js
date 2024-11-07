fcead3f1a2210a67e607ea0098882764006d3e38
Adler Eduardo <adlereduardo@icloud.com>
2024-11-06 23:02:56 -0300

    alterei a altura do container start

--- js/script.js
+++ js/script.js
@@ -36,6 +36,7 @@ closeButton.addEventListener('click', () => {
 
 // Fundo animado //
 
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles-js config loaded');
   });
@@ -81,11 +82,11 @@ AOS.init({
   
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 100, // offset (in px) from the original trigger point
+  offset: 200, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
   duration: 600, // values from 0 to 3000, with step 50ms
   easing: 'ease-in-out', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
+  once: true, // whether animation should happen only once - while scrolling down
   mirror: false, // whether elements should animate out while scrolling past them
   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
